b00ad72b9cacd8a1d05b85a6d579d47d88e7b331
m-shehzad455 <shehzuali5@gmail.com>
2024-01-26 22:31:24 +0500

    update layouts

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import { extendTheme } from '@chakra-ui/react'
-import {mode} from  '@chakra-ui/theme-tools'
-import { BrowserRouter } from 'react-router-dom'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import { BrowserRouter } from "react-router-dom";
 
 const styles = {
 	global: (props) => ({
@@ -25,12 +24,12 @@ const config = {
 // 3. extend the theme
 const theme = extendTheme({ config, styles });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>    
-    <ChakraProvider theme={theme}>
-    <App />
-    </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<ChakraProvider theme={theme}>
+				<App />
+			</ChakraProvider>
+		</BrowserRouter>
+	</React.StrictMode>
+);
